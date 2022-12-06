205ecab21f7b7174a99ef86bf566cafc5eb419a6
Ludwig Pacifici <ludwig@lud.cc>
2022-12-06 05:25:38 +0000

    Added day6

---
+++ day6/src/main.rs
@@ -0,0 +1,21 @@
+use std::{collections::HashSet, error::Error, fs::read_to_string};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_to_string("input.txt")?;
+
+    println!("part 1: {:?}", run(&input, 4));
+    println!("part 2: {:?}", run(&input, 14));
+
+    Ok(())
+}
+
+fn run(input: &str, window_len: usize) -> usize {
+    input
+        .as_bytes()
+        .windows(window_len)
+        .enumerate()
+        .find(|(_, w)| w.iter().collect::<HashSet<_>>().len() == window_len)
+        .unwrap()
+        .0
+        + window_len
+}
